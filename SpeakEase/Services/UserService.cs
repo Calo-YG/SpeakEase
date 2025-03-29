d5a1a3af34639be80c05ce1de8d0733be3fa4466
wyg <3164522206@qq.com>
2025-03-30 01:37:19 +0800

    解决报错，后续实现本地文件存储

--- SpeakEase/Services/UserService.cs
+++ SpeakEase/Services/UserService.cs
@@ -9,7 +9,6 @@ using SpeakEase.Domain.Users;
 using SpeakEase.Domain.Users.Enum;
 using SpeakEase.Infrastructure.EntityFrameworkCore;
 using SpeakEase.Infrastructure.Exceptions;
-using SpeakEase.Infrastructure.Files;
 using SpeakEase.Infrastructure.Filters;
 using SpeakEase.Infrastructure.Redis;
 using SpeakEase.Infrastructure.SpeakEase.Core;
@@ -22,7 +21,7 @@ namespace SpeakEase.Services
     [Filter(typeof(ResultEndPointFilter))]
     [Route("api/user")]
     [Tags("用户服务")]
-    public class UserService(ICaptcha captcha,IDbContext dbContext,IdGenerator idgenerator,IRedisService redisService,IFileProvider fileProvider): FastApi, IUserService
+    public class UserService(ICaptcha captcha,IDbContext dbContext,IdGenerator idgenerator,IRedisService redisService): FastApi, IUserService
     {
         /// <summary>
         /// 用户注册
@@ -156,7 +155,8 @@ namespace SpeakEase.Services
                 ThrowUserFriendlyException.ThrowException("请选择头像上传");
             }
 
-            await fileProvider.
+            await Task.CompletedTask;
+           // await fileProvider.
         }
     }
 }
