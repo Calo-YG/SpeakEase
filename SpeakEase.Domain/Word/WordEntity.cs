824e9896f3e4488ecb8e976a19a441d09b648f8b
wyg <3164522206@qq.com>
2025-05-09 20:34:23 +0800

    新增 WordEntity 类及 LevelEnum 枚举以支持单词管理
    新增命名空间 `SpeakEase.Domain.Word`，定义了 `LevelEnum` 枚举，表示单词等级（初级、中级、高级）。新增领域实体类 `WordEntity`，继承自 `Entity<long>` 并实现 `ICreation` 接口，包含以下属性：`Word`（单词）、`Phonetic`（音标）、`Level`（等级）、`CreateById`（创建人 ID）、`CreateByName`（创建人名称）、`CreatedAt`（创建时间）。该设计支持单词核心信息管理及创建者信息记录。

---
+++ SpeakEase.Domain/Word/WordEntity.cs
@@ -0,0 +1,35 @@
+﻿using SpeakEase.Domain.Shared;
+
+namespace SpeakEase.Domain.Word
+{
+    public class WordEntity :Entity<long>, ICreation
+    {
+        /// <summary>
+        /// 单词
+        /// </summary>
+        public string Word { get; set; }
+
+        /// <summary>
+        /// 音标
+        /// </summary>
+        public string Phonetic { get; set; }
+
+        /// <summary>
+        /// 释义
+        /// </summary>
+        public LevelEnum Level { get; set; }
+
+        /// <summary>
+        /// 创建人id
+        /// </summary>
+        public long CreateById { get; set; }
+        /// <summary>
+        /// 创建人名称
+        /// </summary>
+        public string CreateByName { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+    }
+}
