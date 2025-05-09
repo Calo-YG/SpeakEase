141b8132e2673cc9d7b34048333c56dd0e4ead5e
wenyaoguang <wenyaoguang@163.com>
2025-05-09 17:48:53 +0800

    添加对 SpeakEase.Socail 和 SpeakEase.Gateway 的支持
    在 Program.cs 中集成 SpeakEase_Socail 和 SpeakEase_Gateway 项目，并配置 OpenAPI 服务和默认端点映射。更新了 SpeakEase.Aspire.Host.AppHost.csproj 和 SpeakEase.sln，添加了对新项目的引用和定义。为 appsettings 文件添加了日志记录配置。更新了 SpeakEase.Gateway 和 SpeakEase.Socail 的项目文件，配置了 SDK 和目标框架，并添加了获取天气预报的 HTTP 请求示例。配置了启动设置以支持新的主机。

--- SpeakEase.Aspire.Host/SpeakEase.Aspire.Host.AppHost/Program.cs
+++ SpeakEase.Aspire.Host/SpeakEase.Aspire.Host.AppHost/Program.cs
@@ -2,4 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.SpeakEase>("speakease");
 
+builder.AddProject<Projects.SpeakEase_Socail>("speakease-socail");
+
+builder.AddProject<Projects.SpeakEase_Gateway>("speakease-gateway");
+
 builder.Build().Run();
