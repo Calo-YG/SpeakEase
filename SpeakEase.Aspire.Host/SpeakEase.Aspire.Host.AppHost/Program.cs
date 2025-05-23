c007058a2bf7aa651735608ba2a5aec76157b169
wenyaoguang <wenyaoguang@163.com>
2025-05-23 13:32:14 +0800

    更新项目引用，添加 Study Host 支持
    在 `Program.cs` 中添加了对 `Projects.SpeakEase_Study_Host` 的引用，并在构建后调用 `Run()` 方法。同时，移除了对 `Projects.SpeakEase`、`Projects.SpeakEase_Socail` 和 `Projects.SpeakEase_Gateway` 的引用。
    
    在 `SpeakEase.Aspire.Host.AppHost.csproj` 中新增了对 `SpeakEase.Study.Host.csproj` 的引用，移除了对 `SpeakEase`、`SpeakEase.Gateway` 和 `SpeakEase.Socail` 的引用。

--- SpeakEase.Aspire.Host/SpeakEase.Aspire.Host.AppHost/Program.cs
+++ SpeakEase.Aspire.Host/SpeakEase.Aspire.Host.AppHost/Program.cs
@@ -1,9 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.SpeakEase>("speakease");
 
 builder.AddProject<Projects.SpeakEase_Socail>("speakease-socail");
 
 builder.AddProject<Projects.SpeakEase_Gateway>("speakease-gateway");
+
+builder.AddProject<Projects.SpeakEase_Study_Host>("speakease-study-host");
 
 builder.Build().Run();
