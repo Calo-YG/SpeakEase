5af5e567761499edffa508b1d230f1ef71eb1daa
wenyaoguang <wenyaoguang@163.com>
2025-05-23 18:18:07 +0800

    修复 DefaultGrpcServiceBuildFactory.cs 中的引用
    在 `DefaultGrpcServiceBuildFactory.cs` 文件中，移除了 `using Microsoft.AspNetCore.Http;` 这一行，并重新添加了相同的引用。此更改主要是为了格式化或修复代码，实际导入内容未发生变化。

--- framework/SpeakEase.BuildingBlock.Grpc/Contrib.Grpc/DefaultGrpcServiceBuildFactory.cs
+++ framework/SpeakEase.BuildingBlock.Grpc/Contrib.Grpc/DefaultGrpcServiceBuildFactory.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿
+
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
 using SpeakEase.BuildingBlock.Grpc.BuildingBlock.Grpc;
 using SpeakEase.BuildingBlock.Grpc.BuildingBlock.Grpc.Interface;
